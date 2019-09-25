15201e41c5605fa1e97c4dcfd6643974196f9a55
tiendung69 <111dung1111@gmail.com>
2019-09-25 18:13:32 +0700

    add

---
+++ src/main/java/com/demo/oauth2/controller/LoginController.java
@@ -0,0 +1,38 @@
+
+package com.demo.oauth2.controller;
+
+import com.demo.oauth2.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping(value = "/api/login")
+public class LoginController {
+    
+    @Autowired
+    private  LoginService loginService;
+    
+    @GetMapping(value = "/")
+    @ResponseBody
+    public RedirectView loginPage(){
+        RedirectView redirectView = new RedirectView();
+        System.out.println("asdfasdf");
+        String url = this.loginService.loginFacebook();
+        redirectView.setUrl(url);
+        return redirectView;
+    }
+    
+    @GetMapping("/facebook")
+    @ResponseBody
+    public String callBackUrl(@RequestParam("code") String code){
+        System.out.println("callBackUrl");
+        String tooken = this.loginService.getAccessFacebookToken(code);
+        System.out.println(tooken);
+        return "callBackUrl";
+    }
+}
---
+++ src/main/java/com/demo/oauth2/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.demo.oauth2.service;
+
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    private String appId = "395754711359142";
+    private String appSecret = "6faff585af2595fe6d7e98fe9d374ab7";
+
+    private FacebookConnectionFactory createConnectionFactory() {
+
+        return new FacebookConnectionFactory(appId, appSecret);
+    }
+
+    public String loginFacebook() {
+        OAuth2Parameters parameters = new OAuth2Parameters();
+        parameters.setRedirectUri("localhost:8080/facebook");
+        parameters.setScope("public_profile, email");
+        return createConnectionFactory().getOAuthOperations().buildAuthenticateUrl(parameters);
+    }
+
+    public String getAccessFacebookToken(String code) {
+
+        return createConnectionFactory().getOAuthOperations().exchangeForAccess(code, "localhost:8080/facebook", null).getAccessToken();
+    }
+}
